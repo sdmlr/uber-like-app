111790ca12ff333b23e3d275c15fe6d3fa37f222
sdmlr <sdmlr_@hotmail.com>
2025-02-26 18:45:02 +0000

    create components and onboarding pages

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout() {
   }
 
   return (
-    <Stack>
+    <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index" options={{ headerShown: false }} />
       <Stack.Screen name="(root)" options={{ headerShown: false }} />
       <Stack.Screen name="(auth)" options={{ headerShown: false }} />
