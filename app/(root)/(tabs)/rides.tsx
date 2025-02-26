532d45665870d5fb5dc226832202d30b2961bcf9
sdmlr <sdmlr_@hotmail.com>
2025-02-26 17:05:03 +0000

    Add sign-up flow with EAS build configuration and auth integration to start

---
+++ app/(root)/(tabs)/rides.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Rides = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Rides</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Rides;
