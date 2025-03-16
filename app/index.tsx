665b4bb8fcda15a104e58d6593f9f5028120fbdb
Stefano Di Michele <sdmlr_@hotmail.com>
2025-03-16 16:01:59 +0000

    enable verifation & navigation

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,13 @@
 import { Redirect } from "expo-router";
-import { View, Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { useAuth } from "@clerk/clerk-expo";
 
 const Home = () => {
-  return <Redirect href='/(auth)/welcome' />;
+  const { isSignedIn } = useAuth();
+
+  if (isSignedIn) {
+    return <Redirect href="/(root)/(tabs)/home" />;
+  }
+  return <Redirect href="/(auth)/welcome" />;
 };
 
 export default Home;
