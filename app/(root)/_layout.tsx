f13ac95b279d9c4b890a35ba65c1c82fb2c4d86b
Stefano Di Michele <sdmlr_@hotmail.com>
2025-03-18 23:16:40 +0000

    add more screens to the stack

--- app/(root)/_layout.tsx
+++ app/(root)/_layout.tsx
@@ -4,9 +4,11 @@ const Layout = () => {
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="find-ride" options={{ headerShown: false }} />
+      <Stack.Screen name="confirm-ride" options={{ headerShown: false }} />
+      {/* <Stack.Screen name="book-ride" options={{ headerShown: false }} /> */}
     </Stack>
   );
 };
 
 export default Layout;
-  
