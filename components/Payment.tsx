272943b7d4d3fca34b3c099354d456f6b3029214
Stefano Di Michele <sdmlr_@hotmail.com>
2025-03-19 18:06:45 +0000

    creation of component

---
+++ components/Payment.tsx
@@ -0,0 +1,17 @@
+import { Text, View } from "react-native";
+import CustomButton from "./CustomButton";
+
+const Payment = () => {
+  const openPaymentSheet = async () => {};
+  return (
+    <>
+      <CustomButton
+        title="Confirm Ride"
+        className="my-10"
+        onPress={openPaymentSheet}
+      />
+    </>
+  );
+};
+
+export default Payment;
