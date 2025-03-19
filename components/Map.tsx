1728d063c9c417be1ee747fefd42942023bbfb92
Stefano Di Michele <sdmlr_@hotmail.com>
2025-03-19 13:16:40 +0000

    added time data

--- components/Map.tsx
+++ components/Map.tsx
@@ -17,6 +17,7 @@ const drivers = [
       "https://ucarecdn.com/a2dc52b2-8bf7-4e49-9a36-3ffb5229ed02/-/preview/465x466/",
     car_seats: 4,
     rating: "4.80",
+    time: 87,
   },
   {
     id: "2",
@@ -28,6 +29,7 @@ const drivers = [
       "https://ucarecdn.com/a3872f80-c094-409c-82f8-c9ff38429327/-/preview/930x932/",
     car_seats: 5,
     rating: "4.60",
+    time: 42,
   },
   {
     id: "3",
@@ -39,6 +41,7 @@ const drivers = [
       "https://ucarecdn.com/289764fb-55b6-4427-b1d1-f655987b4a14/-/preview/930x932/",
     car_seats: 4,
     rating: "4.70",
+    time: 59,
   },
   {
     id: "4",
@@ -50,6 +53,7 @@ const drivers = [
       "https://ucarecdn.com/b6fb3b55-7676-4ff3-8484-fb115e268d32/-/preview/930x932/",
     car_seats: 4,
     rating: "4.90",
+    time: 112,
   },
 ];
 
@@ -71,6 +75,8 @@ const Map = () => {
   });
 
   useEffect(() => {
+    // TODO: Remove
+    setDrivers(drivers);
     if (Array.isArray(drivers)) {
       if (!userLatitude || !userLongitude) return;
       const newMarkers = generateMarkersFromData({
