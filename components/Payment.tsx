66fb378a5935697fae56a4b4074fe22da379429d
Stefano Di Michele <sdmlr_@hotmail.com>
2025-03-19 22:58:33 +0000

    set up stripe payment component

--- components/Payment.tsx
+++ components/Payment.tsx
@@ -1,8 +1,116 @@
-import { Text, View } from "react-native";
+import { Alert, Text, View } from "react-native";
 import CustomButton from "./CustomButton";
+import { useStripe } from "@stripe/stripe-react-native";
+import { useEffect, useState } from "react";
+import { PaymentProps } from "@/types/type";
+import { fetchAPI } from "@/lib/fetch";
 
-const Payment = () => {
-  const openPaymentSheet = async () => {};
+const Payment = ({
+  fullName,
+  email,
+  amount,
+  driverId,
+  rideTime,
+}: PaymentProps) => {
+  const { initPaymentSheet, presentPaymentSheet } = useStripe();
+  const [success, setSuccess] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  // const fetchPaymentSheetParams = async () => {
+  //   const response = await fetch(`${API_URL}/payment-sheet`, {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //   });
+  //   const { paymentIntent, ephemeralKey, customer } = await response.json();
+
+  //   return {
+  //     paymentIntent,
+  //     ephemeralKey,
+  //     customer,
+  //   };
+  // };
+
+  const confirmHandler = async (paymentMethod, _, intentCreationCallback) => {
+    const { paymentIntent, customer } = await fetchAPI(
+      "/(api)/(stripe)/create",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: fullName || email.split("@")[0],
+          email: email,
+          amount: amount,
+          paymentMethodId: paymentMethod.id,
+        }),
+      }
+    );
+
+    if (paymentIntent.client_secret) {
+      const { result } = await fetchAPI("/(api)/(stripe)pay", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          payment_method_id: paymentMethod.id,
+          payment_intent_id: paymentIntent.id,
+          customer_id: customer,
+        }),
+      });
+      if(result.client_secret) {
+        // ride/create
+      }
+    }
+
+  };
+
+  const initializePaymentSheet = async () => {
+    // const { paymentIntent, ephemeralKey, customer } =
+    //   await fetchPaymentSheetParams();
+
+    const { error } = await initPaymentSheet({
+      merchantDisplayName: "Example, Inc.",
+      intentConfiguration: {
+        mode: {
+          amount: 1099,
+          currencyCode: "USD",
+        },
+        confirmHandler: confirmHandler,
+      },
+
+      // customerId: customer,
+      // customerEphemeralKeySecret: ephemeralKey,
+      // paymentIntentClientSecret: paymentIntent,
+
+      // Set `allowsDelayedPaymentMethods` to true if your business can handle payment
+      //methods that complete payment after a delay, like SEPA Debit and Sofort.
+      // allowsDelayedPaymentMethods: true,
+      // defaultBillingDetails: {
+      //   name: "Jane Doe",
+      // },
+    });
+    if (!error) {
+      setLoading(true);
+    }
+  };
+
+  const openPaymentSheet = async () => {
+    await initializePaymentSheet();
+    const { error } = await presentPaymentSheet();
+
+    if (error) {
+      Alert.alert(`Error code: ${error.code}`, error.message);
+    } else {
+      setSuccess(true);
+      Alert.alert("Success", "Your order is confirmed!");
+    }
+    console.log("hello from paymentsheet");
+  };
+
   return (
     <>
       <CustomButton
