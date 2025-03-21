effe00a81e92a602f9d8891693b7d15f8f16cf97
Stefano Di Michele <sdmlr_@hotmail.com>
2025-03-21 13:01:17 +0000

    enables google authentication

--- components/OAuth.tsx
+++ components/OAuth.tsx
@@ -1,10 +1,35 @@
-import { Image, Text, View } from "react-native";
+import { Alert, Image, Text, View } from "react-native";
 import CustomButton from "./CustomButton";
 import { icons } from "@/constants";
+import { useSSO } from "@clerk/clerk-expo";
+import { useCallback } from "react";
+import { googleOAuth } from "@/lib/auth";
+import { router } from "expo-router";
 
 const OAuth = () => {
-    const handleGoogleSignIn = async () => {}
+  // Use the `useSSO()` hook to access the `startSSOFlow()` method
+  const { startSSOFlow } = useSSO({
+    strategy: "oauth_google",
+  });
 
+  const handleGoogleSignIn = useCallback(async () => {
+    try {
+      // Start the authentication process by calling `startSSOFlow()`
+      const result = await googleOAuth(startSSOFlow);
+
+      // If sign in was successful, set the active session
+      if (result.code === 'session_exists') {
+        Alert.alert("Sucess", 'Session Exists. Redirecting to home page')
+        router.push('/(root)/(tabs)/home')
+      }
+
+      Alert.alert(result.success ? 'Success' : 'Error', result.message)
+    } catch (err) {
+      // See https://clerk.com/docs/custom-flows/error-handling
+      // for more info on error handling
+      console.error(JSON.stringify(err, null, 2));
+    }
+  }, []);
 
   return (
     <View>
